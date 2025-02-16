17e1ced75283f55a0ea99538eda5c7f6700a9310
SGS-skelton <sgscorporation69@gmail.com>
2025-02-16 23:36:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,5 @@ def download_templates():
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(UPLOAD_FOLDER, filename)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
