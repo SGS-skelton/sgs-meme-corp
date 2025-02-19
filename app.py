879eecbff4a6738e023931d6526b52922d8dff10
Dev <sgscorporation@gmail.com>
2025-02-19 07:24:14 +0530

    add app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder="templates")
 
 # Ensure 'static/uploads' directory exists
 UPLOAD_FOLDER = "static/uploads"
